4657320c43b0e725fda40fbacb71758c500377ea
Samuel Denty <samdenty99@outlook.com>
2017-06-07 01:14:12 +0100

    Bug fixes

--- web_server/html/js/functions.js
+++ web_server/html/js/functions.js
@@ -61,7 +61,7 @@ function getResponse(adr, callback, timeoutCallback, timeout, method){
 	document.getElementsByTagName('head')[0].appendChild(link);
 
 /* Dynamically add footer */
-	document.getElementsByTagName("footer")[0].innerHTML = "<div class=footer><ul><li><a href=https://github.com/spacehuhn target=blank_ style=color:#fff;font-weight:400>Wi-PWN © 2017</a><li><a href=https://github.com/Wi-PWN/Wi-PWN target=blank_>GitHub</a><li><a href=https://github.com/Wi-PWN/Wi-PWN/blob/master/README.md target=blank_>Documentation</a></ul></div><div class=sub-section-attribution><a href=http://mirum.weebly.com/samuel-denty.html target=blank_><b>Samuel Denty</b> @samdenty99</a></div>";
+	document.getElementsByTagName("footer")[0].innerHTML = "<div class=footer><ul><li><a href=https://github.com/spacehuhn target=blank_ style=color:#fff;font-weight:400>Wi-PWN © 2017</a><li><a href=https://github.com/Wi-PWN/Wi-PWN target=blank_>GitHub</a><li><a href=https://github.com/Wi-PWN/Wi-PWN/blob/master/README.md target=blank_>Documentation</a></ul></div><div class=sub-section-attribution><a href=http://mirum.weebly.com/samuel-denty.html target=blank_><b>Sam Denty</b> @samdenty99</a></div>";
 
 /* Dynamically add SVG version of Wi-PWN logo */
 	var svgLogo = '<svg height=100% viewBox="0 0 48 48"width=100%><path d="M47.28 14c-.9-.68-9.85-8-23.28-8-3.01 0-5.78.38-8.3.96l20.66 20.64 10.92-13.6zm-40.73-11.11l-2.55 2.55 4.11 4.11c-4.28 1.97-6.92 4.1-7.39 4.46l23.26 28.98.02.01.02-.02 7.8-9.72 6.63 6.63 2.55-2.55-34.45-34.45z"fill=#29363C></path></svg>'
