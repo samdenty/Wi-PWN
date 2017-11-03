6e7ab08dd7ecfbb8849b00b643e4ecf221596e1b
samdd <sam@samdd.me>
2017-11-03 00:11:21 +0000

    [+] Development debugger

---
+++ web_server/html/debugger.js
@@ -0,0 +1,18 @@
+---
+---
+//  _ _ _ _     _____ _ _ _ _____ 
+// | | | |_|___|  _  | | | |   | |
+// | | | | |___|   __| | | | | | |
+// |_____|_|   |__|  |_____|_|___|
+//                       Debugger
+//
+//       COPYRIGHT SAM DENTY      
+//        https://samdd.me
+
+// Indicate debugger is active
+setTimeout(function(){console.log('%c[debugger@Wi-PWN] %cInjected into process', 'color:#ff5e00', 'color:#e89767')}, 0)
+
+// 
+// TODO: Add button to toggle dark mode & add to cookie
+// Add list which switches between languages
+document.getElementById("darkStyle")
